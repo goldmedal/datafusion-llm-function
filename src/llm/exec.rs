90bd4f666877c8c0695d80cf2b2ceb90d0c4ab9f
Andrew Lamb <andrew@nerdnetworks.org>
2025-01-09 07:22:07 -0500

    Implement general purpose async function evaluation

---
+++ src/llm/exec.rs
@@ -0,0 +1,166 @@
+use crate::llm::async_func::AsyncFuncExpr;
+use datafusion::arrow::datatypes::{Fields, Schema, SchemaRef};
+use datafusion::arrow::record_batch::RecordBatch;
+use datafusion::common::Result;
+use datafusion::execution::{SendableRecordBatchStream, TaskContext};
+use datafusion::physical_expr::EquivalenceProperties;
+use datafusion::physical_plan::metrics::ExecutionPlanMetricsSet;
+use datafusion::physical_plan::stream::RecordBatchStreamAdapter;
+use datafusion::physical_plan::{
+    DisplayAs, DisplayFormatType, ExecutionPlan, ExecutionPlanProperties, Partitioning,
+    PlanProperties,
+};
+use futures::stream::StreamExt;
+use log::trace;
+use std::any::Any;
+use std::sync::Arc;
+
+/// This structure evaluates  a set of async expressions on a record
+/// batch producing a new record batch
+///
+/// This is similar to a ProjectionExec except that the functions can be async
+///
+/// The schema of the output of the AsyncFuncExec is:
+/// Input columns followed by one column for each async expression
+#[derive(Debug)]
+pub struct AsyncFuncExec {
+    /// The async expressions to evaluate
+    async_exprs: Vec<AsyncFuncExpr>,
+    input: Arc<dyn ExecutionPlan>,
+    /// Cache holding plan properties like equivalences, output partitioning etc.
+    cache: PlanProperties,
+    metrics: ExecutionPlanMetricsSet,
+}
+
+impl AsyncFuncExec {
+    pub fn new(async_exprs: Vec<AsyncFuncExpr>, input: Arc<dyn ExecutionPlan>) -> Self {
+        // compute the output schema: input schema then async expressions
+        let fields: Fields = input
+            .schema()
+            .fields()
+            .iter()
+            .cloned()
+            .chain(
+                async_exprs
+                    .iter()
+                    .map(|async_expr| Arc::new(async_expr.field(input.schema().as_ref()))),
+            )
+            .collect();
+        let schema = Arc::new(Schema::new(fields));
+        let cache = AsyncFuncExec::compute_properties(&input, schema).unwrap();
+
+        Self {
+            input,
+            async_exprs,
+            cache,
+            metrics: ExecutionPlanMetricsSet::new(),
+        }
+    }
+
+    /// This function creates the cache object that stores the plan properties
+    /// such as schema, equivalence properties, ordering, partitioning, etc.
+    fn compute_properties(
+        input: &Arc<dyn ExecutionPlan>,
+        schema: SchemaRef,
+    ) -> Result<PlanProperties> {
+        let eq_properties = EquivalenceProperties::new(schema);
+
+        // TODO: This is a dummy partitioning. We need to figure out the actual partitioning.
+        let output_partitioning = Partitioning::RoundRobinBatch(1);
+
+        Ok(PlanProperties::new(
+            eq_properties,
+            output_partitioning,
+            input.pipeline_behavior(),
+            input.boundedness(),
+        ))
+    }
+}
+
+impl DisplayAs for AsyncFuncExec {
+    fn fmt_as(&self, t: DisplayFormatType, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match t {
+            DisplayFormatType::Default | DisplayFormatType::Verbose => {
+                let expr: Vec<String> = self
+                    .async_exprs
+                    .iter()
+                    .map(|async_expr| async_expr.to_string())
+                    .collect();
+
+                write!(f, "AsyncFuncExec: async_expr=[{}]", expr.join(", "))
+            }
+        }
+    }
+}
+
+impl ExecutionPlan for AsyncFuncExec {
+    fn name(&self) -> &str {
+        "async_func"
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn properties(&self) -> &PlanProperties {
+        &self.cache
+    }
+
+    fn children(&self) -> Vec<&Arc<dyn ExecutionPlan>> {
+        vec![&self.input]
+    }
+
+    fn with_new_children(
+        self: Arc<Self>,
+        _children: Vec<Arc<dyn ExecutionPlan>>,
+    ) -> Result<Arc<dyn ExecutionPlan>> {
+        Ok(Arc::new(AsyncFuncExec::new(
+            self.async_exprs.clone(),
+            Arc::clone(&self.input),
+        )))
+    }
+
+    fn execute(
+        &self,
+        partition: usize,
+        context: Arc<TaskContext>,
+    ) -> Result<SendableRecordBatchStream> {
+        trace!(
+            "Start AsyncFuncExpr::execute for partition {} of context session_id {} and task_id {:?}",
+            partition,
+            context.session_id(),
+            context.task_id()
+        );
+        // TODO figure out how to record metrics
+
+        // first execute the input stream
+        let input_stream = self.input.execute(partition, context.clone())?;
+
+        // now, for each record batch, evaluate the async expressions and add the columns to the result
+        let async_exprs_captured = Arc::new(self.async_exprs.clone());
+        let schema_captured = self.schema();
+
+        let stream_with_async_functions = input_stream.then(move |batch| {
+            // need to clone *again* to capture the async_exprs and schema in the
+            // stream and satisfy lifetime requirements.
+            let async_exprs_captured = Arc::clone(&async_exprs_captured);
+            let schema_captured = schema_captured.clone();
+
+            async move {
+                let batch = batch?;
+                // append the result of evaluating the async expressions to the output
+                let mut output_arrays = batch.columns().to_vec();
+                for async_expr in async_exprs_captured.iter() {
+                    let output_array = async_expr.invoke_async(&batch).await?;
+                    output_arrays.push(output_array);
+                }
+                let batch = RecordBatch::try_new(schema_captured, output_arrays)?;
+                Ok(batch)
+            }
+        });
+
+        // Adapt the stream with the output schema
+        let adapter = RecordBatchStreamAdapter::new(self.schema(), stream_with_async_functions);
+        Ok(Box::pin(adapter))
+    }
+}
--- src/llm/functions.rs
+++ src/llm/functions.rs
@@ -1,49 +1,82 @@
+use async_trait::async_trait;
+use datafusion::arrow::array::{ArrayRef, RecordBatch};
 use datafusion::arrow::datatypes::DataType;
 use datafusion::common::internal_err;
 use datafusion::common::Result;
-use datafusion::logical_expr::{
-    ColumnarValue, ScalarUDFImpl, Signature, TypeSignature, Volatility,
-};
+use datafusion::logical_expr::{ColumnarValue, ScalarUDF, ScalarUDFImpl, Signature};
 use std::any::Any;
+use std::fmt::Debug;
+use std::sync::Arc;
 
-/// A scalar UDF that will be bypassed when planning logical plan.
-/// This is used to register the remote function to the context. The function should not be
-/// invoked by DataFusion. It's only used to generate the logical plan and unparsed them to SQL.
-#[derive(Debug)]
-pub struct ByPassScalarUDF {
-    name: String,
-    return_type: DataType,
-    signature: Signature,
+/// A scalar UDF that can invoke using async methods
+///
+/// Note this is less efficient than the ScalarUDFImpl, but it can be used
+/// to register remote functions in the context.
+///
+/// The name is chosen to  mirror ScalarUDFImpl
+#[async_trait]
+pub trait AsyncUDFImpl: Debug + Send + Sync {
+    /// the function cast as any
+    fn as_any(&self) -> &dyn Any;
+
+    /// The name of the function
+    fn name(&self) -> &str;
+
+    /// The signature of the function
+    fn signature(&self) -> &Signature;
+
+    /// The return type of the function
+    fn return_type(&self, _arg_types: &[DataType]) -> Result<DataType>;
+
+    /// Invoke the function asynchronously with the async arguments
+    async fn invoke_async(&self, args: &RecordBatch) -> Result<ArrayRef>;
 }
 
-impl ByPassScalarUDF {
-    pub fn new(name: &str, return_type: DataType) -> Self {
-        Self {
-            name: name.to_string(),
-            return_type,
-            signature: Signature::one_of(
-                vec![TypeSignature::VariadicAny, TypeSignature::Nullary],
-                Volatility::Volatile,
-            ),
-        }
+/// A scalar UDF that must be invoked using async methods
+///
+/// Note this is not meant to be used directly, but is meant to be an implementation detail
+/// for AsyncUDFImpl.
+///
+/// This is used to register remote functions in the context. The function
+/// should not be invoked by DataFusion. It's only used to generate the logical
+/// plan and unparsed them to SQL.
+#[derive(Debug)]
+pub struct AsyncUDF {
+    inner: Arc<dyn AsyncUDFImpl>,
+}
+
+impl AsyncUDF {
+    pub fn new(inner: Arc<dyn AsyncUDFImpl>) -> Self {
+        Self { inner }
+    }
+
+    /// Turn this AsyncUDF into a ScalarUDF, suitable for
+    /// registering in the context
+    pub fn into_scalar_udf(self) -> Arc<ScalarUDF> {
+        Arc::new(ScalarUDF::new_from_impl(self))
+    }
+
+    /// Invoke the function asynchronously with the async arguments
+    pub async fn invoke_async(&self, args: &RecordBatch) -> Result<ArrayRef> {
+        self.inner.invoke_async(args).await
     }
 }
 
-impl ScalarUDFImpl for ByPassScalarUDF {
+impl ScalarUDFImpl for AsyncUDF {
     fn as_any(&self) -> &dyn Any {
         self
     }
 
     fn name(&self) -> &str {
-        &self.name
+        self.inner.name()
     }
 
     fn signature(&self) -> &Signature {
-        &self.signature
+        self.inner.signature()
     }
 
     fn return_type(&self, _arg_types: &[DataType]) -> Result<DataType> {
-        Ok(self.return_type.clone())
+        self.inner.return_type(_arg_types)
     }
 
     fn invoke(&self, _args: &[ColumnarValue]) -> Result<ColumnarValue> {
