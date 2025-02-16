c87c8a99248e5bc46c228702c0be2278e1932d85
Jax Liu <liugs963@gmail.com>
2025-02-16 20:23:30 +0800

    add tests

---
+++ src/llm/functions/async_upper.rs
@@ -0,0 +1,75 @@
+use crate::llm::async_func::AsyncScalarFunctionArgs;
+use crate::llm::functions::AsyncScalarUDFImpl;
+use async_trait::async_trait;
+use datafusion::arrow::array::{ArrayIter, ArrayRef, AsArray, RecordBatch, StringArray};
+use datafusion::arrow::datatypes::DataType;
+use datafusion::common::internal_err;
+use datafusion::common::types::{logical_int64, logical_string};
+use datafusion::common::Result;
+use datafusion::config::ConfigOptions;
+use datafusion::logical_expr::{
+    ColumnarValue, Signature, TypeSignature, TypeSignatureClass, Volatility,
+};
+use log::trace;
+use std::any::Any;
+use std::sync::Arc;
+
+#[derive(Debug)]
+pub struct AsyncUpper {
+    signature: Signature,
+}
+
+impl AsyncUpper {
+    pub fn new() -> Self {
+        Self {
+            signature: Signature::new(
+                TypeSignature::Coercible(vec![TypeSignatureClass::Native(logical_string())]),
+                Volatility::Volatile,
+            ),
+        }
+    }
+}
+
+#[async_trait]
+impl AsyncScalarUDFImpl for AsyncUpper {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn name(&self) -> &str {
+        "async_upper"
+    }
+
+    fn signature(&self) -> &Signature {
+        &self.signature
+    }
+
+    fn return_type(&self, _arg_types: &[DataType]) -> Result<DataType> {
+        Ok(DataType::Utf8)
+    }
+
+    async fn invoke_async(&self, _args: &RecordBatch) -> Result<ArrayRef> {
+        unimplemented!("This function should not be called")
+    }
+
+    async fn invoke_async_with_args(
+        &self,
+        args: AsyncScalarFunctionArgs,
+        _option: &ConfigOptions,
+    ) -> Result<ArrayRef> {
+        trace!("Invoking async_upper with args: {:?}", args);
+        let value = &args.args[0];
+        let result = match value {
+            ColumnarValue::Array(array) => {
+                let string_array = array.as_string::<i32>();
+                let iter = ArrayIter::new(string_array);
+                let result = iter
+                    .map(|string| string.map(|s| s.to_uppercase()))
+                    .collect::<StringArray>();
+                Arc::new(result) as ArrayRef
+            }
+            _ => return internal_err!("Expected a string argument, got {:?}", value),
+        };
+        Ok(result)
+    }
+}
